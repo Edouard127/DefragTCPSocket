f5cfaf282ba21b591eb9657c642a284e9146145a
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-08-04 22:02:56 -0400

    Refactor: string -> numbers

--- utils/commands.js
+++ utils/commands.js
@@ -1,8 +1,9 @@
 export const bits = {
-    EXIT: "-1",
-    INIT: "0",
-    CONNECT: "2",
-    DISCONNECT: "4",
-    CHAT: "8",
-    BARITONE: "16",
+    EXIT: -1 << 0,
+    INIT: 0 << 0,
+    CONNECT: 1 << 1,
+    DISCONNECT: 1 << 2,
+    CHAT: 2 << 2,
+    BARITONE: 2 << 2,
+    MOD_COMMAND: 2 << 3,
 }
