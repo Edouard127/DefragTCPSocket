52c0b47fe3c1628e4355754f3b5923b8736d2c5b
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-08-06 16:55:45 -0400

    Update commands.ts

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -1,9 +1,10 @@
 export const bits = {
-    EXIT: -1 << 0,
-    INIT: 0 << 0,
-    CONNECT: 1 << 1,
-    DISCONNECT: 1 << 2,
-    CHAT: 2 << 2,
-    BARITONE: 2 << 2,
-    MOD_COMMAND: 2 << 3,
+    EXIT: ~0x0,
+    INIT: 0x0,
+    CONNECT: 0x1,
+    ADD_WORKER: 0x2,
+    DISCONNECT: 0x3,
+    CHAT: 0x4,
+    BARITONE: 0x5,
+    MOD_COMMAND: 0x6,
 } as const;
