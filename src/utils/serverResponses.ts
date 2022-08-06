09a029aa35411a92766ee277aff8d5cdf11c37e6
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-08-06 19:25:59 -0400

    Timeout response

--- src/utils/serverResponses.ts
+++ src/utils/serverResponses.ts
@@ -2,10 +2,13 @@ export const responses = {
     DISCONNECT: "-1",
     UNAUTHORIZED: "0",
     OK: "1",
-    SERVER_CLOSED: "2",
-    SERVER_ERROR: "3",
-    BAD_COMMAND: "4",
-    BAD_ARGUMENTS: "5",
-    BAD_PASSWORD: "6",
-    WORKER_NOT_FOUND: "7",
+    TIMEOUT: "2",
+    SERVER_CLOSED: "3",
+    SERVER_ERROR: "4",
+    BAD_COMMAND: "5",
+    BAD_ARGUMENTS: "6",
+    BAD_PASSWORD: "7",
+    WORKER_NOT_FOUND: "8",
 } as const;
+
+export type ServerResponses = typeof responses[keyof typeof responses];
