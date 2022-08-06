fac444d2b38ffc2cee3b026113fb5cfc0a7ebc0f
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-08-06 19:03:24 -0400

    Feature: Heartbeat

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -1,10 +1,11 @@
 export const bits = {
     EXIT: ~0x0,
-    LOGIN: 0x0,
-    LOGOUT: 0x1,
-    ADD_WORKER: 0x2,
-    REMOVE_WORKER: 0x3,
-    CHAT: 0x4,
-    BARITONE: 0x5,
-    MOD_COMMAND: 0x6,
+    HEARTBEAT: 0x0,
+    LOGIN: 0x1,
+    LOGOUT: 0x2,
+    ADD_WORKER: 0x3,
+    REMOVE_WORKER: 0x4,
+    CHAT: 0x5,
+    BARITONE: 0x6,
+    MOD_COMMAND: 0x7,
 } as const;
