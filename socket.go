d8c1eb275b432e7d3363100d22c3b698bd74f9da
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-08-14 23:27:16 -0400

    Added more packets, wip: keep alive

--- socket.go
+++ socket.go
@@ -5,8 +5,8 @@ import (
 	"github.com/fatih/color"
 	"log"
 	"net"
-	"os"
 	"strings"
+	"time"
 )
 
 const (
@@ -18,7 +18,6 @@ func main() {
 	listener, err := net.Listen("tcp", HOST+":"+PORT)
 	if err != nil {
 		fmt.Println(err)
-		os.Exit(1)
 	}
 	defer func(listener net.Listener) {
 		err := listener.Close()
@@ -26,40 +25,36 @@ func main() {
 
 		}
 	}(listener)
-	color.Cyan("Listening on " + HOST + ":" + PORT)
+	color.New(color.BgCyan, color.Bold).Println("Server started on:", HOST+":"+PORT)
 
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println(err, "error")
 			continue
 		}
+
+		// Execute the keepAlive function in a new goroutine
+		go keepAlive()
+		// Execute the handleRequest function in a new goroutine
 		go handleRequest(conn)
 	}
 }
 
 func handleRequest(conn net.Conn) {
-	buffer := make([]byte, 32)
+	color.New(color.FgGreen, color.Bold).Println("New connection from:", conn.RemoteAddr().String())
+	buffer := make([]byte, 1024)
 
-	_, err := conn.Read(buffer)
+	i, err := conn.Read(buffer)
+	buffer = buffer[:i]
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Slice the buffer if byte 0 is found.
-	for b := range buffer {
-		fmt.Println(b)
-		if buffer[b] == 0 {
-			buffer = buffer[:b]
-			break
-		}
-	}
-	// Check if the buffer is empty.
+
 	if len(buffer) == 0 {
 		conn.Write([]byte{Packets["ERROR"]})
 		conn.Close()
 	}
-
-	color.Green("Connection etablished to: " + conn.RemoteAddr().String())
 
 	// Store the request data in a splited array.
 	request := strings.Fields(string(buffer))
@@ -68,24 +63,36 @@ func handleRequest(conn net.Conn) {
 	// Store the command in a ClientCommands struct.
 	command := ClientCommands{toByte(request[0]), args}
 
-	// Check if the argument at the index 1 contains not null bytes.
-	if args[1] != nil {
-		// If it does, convert it to a string.
-		args[1] = []byte(getString(args[1]))
-	}
+	fmt.Println(command.GetPacketName(), command)
 
-	switch GetNumber(command.Byte) {
-	case 0x04:
+	switch getByteNumber(command.Byte) {
+	case 0x05:
 		client := Client{getString(args[0]), conn, getString(args[1])}
 		clients = append(clients, &client)
 		fmt.Println(client)
 		_, err := conn.Write([]byte{Packets["OK"]})
 		if err != nil {
+			log.Fatal(err)
 			return
 		}
 		break
 	}
 
+}
+
+// Keep clients alive
+func keepAlive() {
+	for {
+		for client := range clients {
+			clients[client].Conn.SetDeadline(time.Now().Add(time.Second * 5))
+			_, err := clients[client].Conn.Write([]byte{Packets["HEARTBEAT"]})
+			if err != nil {
+				clients = append(clients[:client], clients[client+1:]...)
+				return
+			}
+		}
+		time.Sleep(time.Second * 5)
+	}
 }
 
 func broadcast(message []byte) {
@@ -126,8 +133,8 @@ func getString(args []byte) string {
 }
 
 // Get number from byte
-func GetNumber(s byte) int {
-	return int(s - 0x30)
+func getByteNumber(s byte) byte {
+	return s - 0x2C
 }
 
 var clients []*Client
@@ -145,16 +152,16 @@ var Packets = map[string]byte{
 	"EXIT":              0x00, // user->server->client Notifies the client that the server is closing the connection.
 	"OK":                0x01, // client<->server Notifies the client that the server is ready to receive the next packet.
 	"HEARTBEAT":         0x02, // client<->server Ping packet.
-	"LOGIN":             0x03, // user->server<->client Notifies the server that the client is trying to login.
-	"LOGOUT":            0x04, // user->server<->client Notifies the server that the client is trying to logout.
-	"ADD_WORKER":        0x05, // client<->server Notifies the server of a new worker.
-	"REMOVE_WORKER":     0x06, // user<->server<->client Notifies the server that a worker has been removed.
+	"LOGIN":             0x03, // user->server<->client Notifies the server that the client is trying to log in.
+	"LOGOUT":            0x04, // user->server<->client Notifies the server that the client is trying to log out.
+	"ADD_WORKER":        0x05, // user<->server Notifies the server of a new worker.
+	"REMOVE_WORKER":     0x06, // user<->server Notifies the server that a worker has been removed.
 	"GET_WORKERS":       0x07, // user<->server<->client Notifies the server that the user wants to get the list of workers.
 	"GET_WORKER_STATUS": 0x08, // user<->server<->client Notifies the server that the user wants to get the status of a worker.
 	"CHAT":              0x09, // user->server<->client Notifies the server that the user wants to send a chat message.
-	"BARITONE":          0xA0, // user->server<->client Notifies the server that the user wants to send a baritone command.
-	"LAMBDA":            0xA1, // user->server<->client Notifies the server that the user wants to send a lambda command.
-	"ERROR":             0xA2, // client<->server<->user Notifies the user that the server or the client has encountered an error.
+	"BARITONE":          0x0A, // user->server<->client Notifies the server that the user wants to send a baritone command.
+	"LAMBDA":            0x0B, // user->server<->client Notifies the server that the user wants to send a lambda command.
+	"ERROR":             0x0C, // client<->server<->user Notifies the user that the server or the client has encountered an error.
 }
 
 type ServerResponse struct {
@@ -171,7 +178,7 @@ type ClientCommands struct {
 // GetPacketName Get the packet name from the ClientCommands byte.
 func (c *ClientCommands) GetPacketName() string {
 	for k, v := range Packets {
-		if v == c.Byte {
+		if v == getByteNumber(c.Byte) {
 			return k
 		}
 	}
