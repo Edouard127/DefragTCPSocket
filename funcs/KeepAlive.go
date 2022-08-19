35c7e82c5c60ce10938c6bc9254d7e35a0294b35
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-08-18 21:49:15 -0400

    Fix: KeepAlive dereference

--- funcs/KeepAlive.go
+++ funcs/KeepAlive.go
@@ -8,12 +8,12 @@ import (
 // KeepAlive Keep clients alive
 func KeepAlive() {
 	for {
-		clients := structs.Clients
-		for client := range clients {
-			clients[client].Conn.SetDeadline(time.Now().Add(time.Second * 5))
-			_, err := clients[client].Conn.Write([]byte{'\n'})
+		clients := &structs.Clients
+		for client := range *clients {
+			(*clients)[client].Conn.SetDeadline(time.Now().Add(time.Second * 5))
+			_, err := (*clients)[client].Conn.Write([]byte{'\n'})
 			if err != nil {
-				clients = append(clients[:client], clients[client+1:]...)
+				*clients = append((*clients)[:client], (*clients)[client+1:]...)
 				return
 			}
 		}
