f8159365181a77e8052ecefeb2383930d9bab975
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-08-23 17:49:29 -0400

    Update client.js

--- client.js
+++ client.js
@@ -1,15 +1,15 @@
 const net = require("net")
+require("colors")
 
 const client = net.createConnection({
     host: "localhost",
     port: 1984
 }, () => {
-    console.log("connected to server")
+    console.log("Connected to server".green)
     client.write(Buffer.from("13 0"))
     process.stdin.pipe(client)
     client.on("data", (data) => {
         console.log(data.toString())
     })
 })
-// Send hello in bytes
 
