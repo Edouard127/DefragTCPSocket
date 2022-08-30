ef6aae9a2749a53163844525a44ef3658ba53e9e
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-08-30 18:02:01 -0400

    Feature: Get command string

--- structs/ClientCommand.go
+++ structs/ClientCommand.go
@@ -1,6 +1,12 @@
 package structs
 
+import "kamigen/socket/utils"
+
 type ClientCommand struct {
+	// The length of the command.
+	Length byte
+	// Fragmentation byte.
+	Fragmented byte
 	// The byte of the command.
 	Byte byte
 	// The flags of the command.
@@ -17,4 +23,9 @@ func (c *ClientCommand) GetPacketName() string {
 		}
 	}
 	return "ERROR"
+}
+
+// GetCommand Get the command from the ClientCommands bytes.
+func (c *ClientCommand) GetCommand() string {
+	return string(c.Length) + " " + string(c.Fragmented) + " " + string(c.Byte) + " " + string(c.Flag) + " " + string(utils.ByteArraysExtract(c.Args))
 }
