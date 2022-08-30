aff60ef6e2119826d9a456d0a9cffb8eb1f2b0e1
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-08-30 18:00:05 -0400

    Create IOLogs.go

---
+++ utils/IOLogs.go
@@ -0,0 +1,44 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func LogFile(con bool, message ...string) error {
+	if _, err := os.Stat("log.txt"); os.IsNotExist(err) {
+		f, err := os.Create("log.txt")
+		if err != nil {
+			return err
+		}
+		defer func(f *os.File) {
+			err := f.Close()
+			if err != nil {
+
+			}
+		}(f)
+	}
+	if f, err := os.OpenFile("log.txt", os.O_APPEND|os.O_WRONLY, 0644); err != nil {
+		return err
+	} else {
+		defer func(f *os.File) {
+			err := f.Close()
+			if err != nil {
+
+			}
+		}(f)
+		for _, v := range message {
+			date := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), time.Now().Hour(), time.Now().Minute(), time.Now().Second(), time.Now().Nanosecond(), time.Local)
+			if s, err := f.WriteString(date.String() + ": " + v + " "); err != nil {
+				return err
+			} else if con {
+				fmt.Println(s)
+			}
+		}
+		if _, err := f.WriteString("\n"); err != nil {
+			return err
+		}
+		return nil
+	}
+}
